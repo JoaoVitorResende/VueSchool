c88154b4d046c0c78c9480e356e32874fdb5e2b7
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2024-12-16 20:04:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Alunos/>
+</template>
+
+<script>
+import Alunos from './components/AlunosClass.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Alunos
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/AlunosClass.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+   <h1>{{titulo}}</h1>
+   <input type="text" placeholder="Nome do aluno" 
+   v-model="name" v-on:keyup.enter="addAluno()">
+   <table border="1px">
+    <thead>
+      <th>Mataterias</th>
+      <th>Nomes</th>
+      <th>Opções</th>
+    </thead>
+    <tbody>
+      <tr v-for="(aluno,index) in alunos" :key="index">
+        <td>{{aluno.id}}</td>
+        <td>{{aluno.name}}</td>
+        <td>
+          <button class="btn" @click="remover()">Remover</button>
+        </td>
+      </tr>
+    </tbody>
+   </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data()
+  {
+    return {
+      titulo:"Aluno",
+      name: "",
+      alunos: [{id:1,name:"jhon"},{id:2,name:"Lucas"},{id:3,name:"jhonathan"},]
+      }
+  },
+  props: {
+
+  },
+  methods: {
+    addAluno() {
+      let aluno = { 
+        id: this.alunos.length + 1,
+        name: this.name
+      }
+      this.alunos.push(aluno);
+      this.name = "";
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
