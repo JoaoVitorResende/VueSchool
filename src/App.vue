6c35d41b3553339b43f25ac87d00bf7291701d4f
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2024-12-28 10:06:21 -0300

    adição de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,55 @@
 <template>
-  <div id='App'>
+  <div id="app" >
     <Nav></Nav>
-    <Alunos/>
-    <professores/>
-    <sobre/>
+    <div class="marginPrincipal">
+      <router-view :key="$route.fullPath"></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import Alunos from './components/Aluno/AlunosClass.vue'
+
 import Nav from './components/_Nav/navClass.vue'
-import Professores from './components/Professor/ProfessorClass.vue'
-import Sobre from './components/Sobre/SobreClass.vue'
+
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    Alunos,
-    Nav,
-    Professores,
-    Sobre
+    Nav
   }
 }
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
 body{
-  background-color: azure;
-  font-family: Montserrat, sans-serif;
+  background-color: #eee;
+  font-family: "Montserrat", sans-serif;
   display: grid;
-  justify-content: center;
+  justify-items: center;
 }
-body, html{
+body, html {
   margin: 0;
   height: 100%;
 }
-.marginPrincipal{
-  width: 50%;
-  margin: auto;
+.marginPrincipal {
+     width: 50%;
+     margin: auto;
 }
-#App {
-  width: 100%;
+#app {
+  width: 100%
 }
-.btn_danger{
-  background-color: red;
+
+.btn_Danger {
+  font-size: 0.8em !important;
+  background-color: #fa4430;
 }
-.btn{
+
+.btn{  
   padding: 10px 20px;
   cursor: pointer;
-  border: 1px solid black;
+  border: 1px solid #000;
+  font-size: 1.2em;
   color: white;
   font-weight: bold;
   border-radius: 5px;
@@ -59,26 +60,30 @@ body, html{
   margin-top: 3px;
   border-bottom: 1px solid black;
 }
-table{
-  margin: 0;
-  padding: 0;
+
+table {
+  margin: 0px;
+  padding: 0px;
   list-style-type: none;
   width: 100%;
 }
 table tr td{
   padding: 20px;
   font-size: 1.3em;
-  background-color: #e0e0e0;
-  margin-bottom:2px;
+  background-color: #e0edf4;
+  margin-bottom: 2px;
   color: #3e5252;
 }
 table thead th{
-  background-color: rgb(23, 158, 202) !important;
+  background-color: rgb(184, 208, 216) !important;
   font-size: 1.2em;
   padding: 10px 0px;
-  text-align: center;
+  text-align: center !important;
 }
-.colPequeno{
+.colPequeno {
   width: 5%;
+  text-align: right;
+  background-color: rgb(125, 217, 245);
+  font-weight: bold;
 }
 </style>
