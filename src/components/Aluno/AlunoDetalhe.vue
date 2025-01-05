2a510e345bf8cb5a36dc181300cd2ecf9843f881
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-01-05 08:31:28 -0300

    Adição de modificações em alunos

--- src/components/Aluno/AlunoDetalhe.vue
+++ src/components/Aluno/AlunoDetalhe.vue
@@ -1,27 +1,52 @@
 <template>
   <div>
-    <TitulosClass :text="aluno?.name ? `Aluno: ${aluno.name}` : 'Carregando...'" />
+    <TitulosClass
+      :text="aluno?.name ? `Aluno: ${aluno.name}` : 'Carregando...'"
+    />
+    <button @click="editAluno()">Editar</button>
     <table>
       <tbody>
         <tr>
-          <td class = "colPequeno">Matrícula:</td>
-          <td></td>
+          <td class="colPequeno">Matrícula:</td>
+          <td>
+            <label>{{ aluno.id }}</label>
+          </td>
         </tr>
         <tr>
-          <td class = "colPequeno">Nome:</td>
-          <td>{{id}}</td>
+          <td class="colPequeno">Nome:</td>
+          <td>
+            <label>{{ aluno.name }}</label>
+            <input  v-if="this.isEditing == true" v-model="aluno.name" type="text" />
+          </td>
         </tr>
         <tr>
-          <td class = "colPequeno">Sobrenome:</td>
-          <td></td>
+          <td class="colPequeno">Sobrenome:</td>
+          <td>
+            <label>{{ aluno.lastName }}</label>
+            <input v-if="this.isEditing == true" v-model="aluno.lastName" type="text" />
+          </td>
         </tr>
         <tr>
-          <td class = "colPequeno">Data de nascimento:</td>
-          <td></td>
+          <td class="colPequeno">Data de nascimento:</td>
+          <td>
+            <label>{{ aluno.data }}</label>
+            <input v-if="this.isEditing == true" v-model="aluno.data" type="text" />
+          </td>
         </tr>
         <tr>
-          <td class = "colPequeno">Professor:</td>
-          <td></td>
+          <td class="colPequeno">Professor:</td>
+          <td>
+            <label>{{ aluno.professor.name }}</label>
+            <select v-if="this.isEditing == true" v-model="aluno.professor">
+              <option
+                v-for="(prfessor, index) in professores"
+                :key="index"
+                v-bind:value="prfessor"
+              >
+                {{ prfessor.name }}
+              </option>
+            </select>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -31,26 +56,39 @@
 <script>
 import TitulosClass from "../_Share/TitulosClass.vue";
 export default {
-    components: {
+  components: {
     TitulosClass,
   },
   data() {
     return {
+      professores: [],
       aluno: {},
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      isEditing: false
     };
   },
   created() {
-    this.getAlunosByID()
+    this.getAlunosByID();
+    this.getProfessores();
   },
   methods: {
     getAlunosByID() {
       this.$http
         .get("http://localhost:3000/alunos/" + this.id)
-        .then(rest => rest.json())
-        .then(alunosApi => (this.aluno = alunosApi));
+        .then((rest) => rest.json())
+        .then((alunosApi) => (this.aluno = alunosApi));
     },
-  }
+    getProfessores() {
+      this.$http
+        .get("http://localhost:3000/professores")
+        .then((rest) => rest.json())
+        .then((professoresApi) => (this.professores = professoresApi));
+    },
+    editAluno()
+    {
+      this.isEditing = !this.isEditing
+    }
+  },
 };
 </script>
 
--- src/components/Professor/ProfessorClass.vue
+++ src/components/Professor/ProfessorClass.vue
@@ -67,7 +67,7 @@ export default {
         }
         this.professores[index] = professor;
       })
-    }
+    },
   },
 };
 </script>
